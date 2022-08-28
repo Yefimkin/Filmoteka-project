39e97845951cadf189a849fa889f46f6cd53c2e7
Denys Yefimkin <iefimkindenis@gmail.com>
2022-08-28 10:38:58 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import './sass/main.scss';
 import '../node_modules/basiclightbox/dist/basicLightbox.min.css';
 import './js/_button-container';
+
+import './js/filmApiService';
+import './js/searchingMovies';
+import './js/trendingFilms';
