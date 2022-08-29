7fa2a29ce338932b5a38aa108d08d468a94b0361
VladRosolov <rosolovvlad@gmail.com>
2022-08-29 21:59:45 +0300

    bugfix

--- src/js/modal.js
+++ src/js/modal.js
@@ -2,33 +2,43 @@ import * as basicLightbox from 'basiclightbox';
 import { FilmApiService } from './filmApiService';
 
 const refs = {
-    filmsList: document.querySelector('.films-list'),
-}
+  filmsList: document.querySelector('.films-list'),
+};
 const filmApiService = new FilmApiService();
 
 refs.filmsList.addEventListener('click', onClickFilm);
 
 async function onClickFilm(e) {
-    if (e.target.tagName !== 'IMG') return;
+  if (e.target.tagName !== 'IMG') return;
 
-    const filmID = e.target.parentElement.dataset.id;
-    const url = "https://image.tmdb.org/t/p/w500/";
+  const filmID = e.target.parentElement.dataset.id;
+  const url = 'https://image.tmdb.org/t/p/w500/';
 
-    try {
-        filmApiService.ID = filmID;
-        const responceFilmID = await filmApiService.fetchMovieByID();
-        console.log(responceFilmID)
-        const { original_title, genres, runtime, popularity, vote_average, vote_count, poster_path, overview } = responceFilmID;
-        let moviePoster = `${url}${poster_path}`;
-   
-        if (!poster_path) {
-            moviePoster = posterNotFound;
-        }
+  try {
+    filmApiService.ID = filmID;
+    const responceFilmID = await filmApiService.fetchMovieByID();
+    console.log(responceFilmID);
+    const {
+      original_title,
+      genres,
+      runtime,
+      popularity,
+      vote_average,
+      vote_count,
+      poster_path,
+      overview,
+    } = responceFilmID;
+    let moviePoster = `${url}${poster_path}`;
 
-        const modalFilm = basicLightbox.create(`
+    if (!poster_path) {
+      moviePoster = posterNotFound;
+    }
+
+    const modalFilm = basicLightbox.create(
+      `
             <div class="modal">
                 <div class="modal-image">
-                <img src="${moviePoster}" alt="Poster for the movie" />
+                <img class="modal-image__el" src="${moviePoster}" alt="Poster for the movie" />
                 </div>
                 <div class="modal-post">
                     <h2 class="modal-post__title">${original_title}</h2>
@@ -37,14 +47,18 @@ async function onClickFilm(e) {
                             <tr class="tr">
                                 <td class="tt">Vote / Votes</td>
                                 <td class="ti">
-                                <span class="vote-value">${vote_average.toFixed(1)}</span>
+                                <span class="vote-value">${vote_average.toFixed(
+                                  1
+                                )}</span>
                                 /
                                 <span class="votes-value">${vote_count}</span>
                                 </td>
                             </tr>
                             <tr class="tr">
                                 <td class="tt">Popularity</td>
-                                <td class="ti popularity-value">${popularity.toFixed(1)}</td>
+                                <td class="ti popularity-value">${popularity.toFixed(
+                                  1
+                                )}</td>
                             </tr>
                             <tr class="tr">
                                 <td class="tt">Original Title</td>
@@ -52,7 +66,9 @@ async function onClickFilm(e) {
                             </tr>
                             <tr class="tr">
                                 <td class="tt">Genre</td>
-                                <td class="ti genre-value">${showGenres(genres)}</td>
+                                <td class="ti genre-value">${showGenres(
+                                  genres
+                                )}</td>
                             </tr>
                         </table>
                     </div>
@@ -70,34 +86,29 @@ async function onClickFilm(e) {
                         <use href="./images/icons.svg#close-modal"></use>
                     </svg></a>
                 </button>
-            </div>`, {
-            onShow: (instance) => {
-                    instance.element().querySelector('a').onclick = instance.close;
-            }
-        })
+            </div>`,
+      {
+        onShow: instance => {
+          instance.element().querySelector('a').onclick = instance.close;
+        },
+      }
+    );
 
-        modalFilm.show();
-
-
-    } catch (error) {
-        console.log(error);
-        }
-
-    
+    modalFilm.show();
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-
 function showGenres(genresArray) {
-    if (genresArray.length > 2) {
-        const genresNameArr = genresArray
-            .map(genre => genre.name)
-            .slice(0, 2)
-            .join(', ');
-        return genresNameArr + ", Other";
-    } else {
-        const genresNameArr = genresArray
-            .map(genre => genre.name)
-            .join(', ');
-        return genresNameArr;
-    }
+  if (genresArray.length > 2) {
+    const genresNameArr = genresArray
+      .map(genre => genre.name)
+      .slice(0, 2)
+      .join(', ');
+    return genresNameArr + ', Other';
+  } else {
+    const genresNameArr = genresArray.map(genre => genre.name).join(', ');
+    return genresNameArr;
+  }
 }
