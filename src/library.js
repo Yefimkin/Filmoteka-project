9f865845ae5592f61ea5540444cfb72a7c3198b6
Denys Yefimkin <iefimkindenis@gmail.com>
2022-08-31 13:17:29 +0300

    Update library.js

--- src/library.js
+++ src/library.js
@@ -2,3 +2,4 @@ import './sass/main.scss';
 import '../node_modules/basiclightbox/dist/basicLightbox.min.css';
 import './js/button-container';
 import './js/libraryTest';
+import './js/footer-modal';
